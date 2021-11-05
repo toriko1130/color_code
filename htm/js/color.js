f0ea26b915dfca94f35d5d9f0a60cec56fd106b8
tori1015 <gustave1015@gmail.com>
2021-11-05 19:10:28 +0800

    JS

---
+++ htm/js/color.js
@@ -0,0 +1,22 @@
+var colorCode = [
+	{
+		colorName:"玉紅",
+		colorHex:"#c04851",
+		rgbR:"192",
+		rgbG:"72",
+		rgbB:"81",
+		cmykC:"13",
+		cmykM:"83",
+		cmykY:"62",
+		cmykK:"3",
+
+
+	}
+];
+
+
+
+/*  =====================  init  ========================  */
+$(document).ready(function() {
+
+});
